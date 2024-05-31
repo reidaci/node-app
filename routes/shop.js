0c2cbfb70471c97c9cc673cef805c66e33b40354
Rei Daci <reiz.daci@outlook.com>
2024-05-31 10:46:02 +0200

    main layout (pug)

--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,15 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
+const express = require('express');
 
-const rootDir = require("../util/path");
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
-const adminData = require("./admin");
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render("shop", { prods: products, docTitle: "Shop" });
+  res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
 });
+
 module.exports = router;
