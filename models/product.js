c91212343b4cdafa74fb8f8e4593e40cf470fe7f
Rei Daci <reiz.daci@outlook.com>
2024-06-04 11:02:05 +0200

    refactor navigation

--- models/product.js
+++ models/product.js
@@ -1,40 +1,37 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+
 const p = path.join(
-    path.dirname(process.mainModule.filename),
-    'data',
-    'products.json'  
-    )
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const getProductsFromFile = (cb) => {
-
-    fs.readFile(p, (err, fileContent)=> {
-        if(err) {
-           cb([]);
-        }  else {
-            cb(JSON.parse(fileContent))
-        }
-        
-    })
-}
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-      getProductsFromFile(products => {
-        products.push(this);
-        fs.writeFile(p,JSON.stringify(products), err => {
-            console.log(err);
-        })
-       })
-    
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(cb) {
-       getProductsFromFile(cb)
-   
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
